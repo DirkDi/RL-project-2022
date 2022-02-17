042f0454fc4f2ef757f85bd0a7b5b41bfd7dc0bf
markossta <markos.stamatakis@yahoo.de>
2022-02-18 00:16:53 +0100

    implemented possible agents from sb3

---
+++ sb_agents.py
@@ -0,0 +1,40 @@
+from stable_baselines3 import SAC, PPO, DQN, A2C
+
+
+def sac_agent(env, total_timesteps=1000, log_interval=10, verbose=1):
+    model = SAC("MlpPolicy", env, verbose=verbose)
+    model.learn(total_timesteps=total_timesteps, log_interval=log_interval)
+    # model.save('sac_city')
+    return model
+
+
+def ppo_agent(env, total_timesteps=1000, log_interval=10, verbose=1):
+    model = PPO("MlpPolicy", env, verbose=verbose)
+    model.learn(total_timesteps=total_timesteps, log_interval=log_interval)
+    # model.save('ppo_city')
+    return model
+
+
+def dqn_agent(env, total_timestamps=1000, log_interval=10, verbose=1):
+    model = DQN("MlpPolicy", env, verbose=verbose)
+    model.learn(total_timesteps=total_timestamps, log_interval=log_interval)
+    # model.save('dqno_city')
+    return model
+
+
+def a2c_agent(env, total_timestamps=1000, log_interval=10, verbose=1):
+    model = A2C("MlpPolicy", env, verbose=verbose)
+    model.learn(total_timesteps=total_timestamps, log_interval=log_interval)
+    # model.save('a2c_city')
+    return model
+
+
+def run_agent(env, model):
+    obs = env.reset()
+    actions = []
+    while True:  # TODO check if loop is correct for all agents
+        action, _states = model.predict(obs, deterministic=True)
+        actions.append(action)
+        obs, reward, done, info = env.step(action)
+        if done:
+            return actions
