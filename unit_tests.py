1ab2da9ae46d3f7f8af05fbf866df6a02eaf8363
markossta <markos.stamatakis@yahoo.de>
2022-03-01 19:49:04 +0100

    added function headers for unittests

--- unit_tests.py
+++ unit_tests.py
@@ -7,6 +7,7 @@ class Test(unittest.TestCase):
     """
     This class is used to test the correctness of implementations (unittests).
     """
+
     def test_validate_accessibility(self):
         """
         Checks if the validation of accessibility is correct.
@@ -46,15 +47,52 @@ class Test(unittest.TestCase):
         self.assertFalse(env.validate_accessibility(3, 0))  # No Path exists because edges are unidirectional
         self.assertFalse(env.validate_accessibility(0, 2))  # No path exists
 
+    def test_weighted_map(self):
+        """
+        Checks if the weighted map is correctly created from the distance matrix, traffic matrix and traffic lights.
+        """
+        # TODO: think about tests
+        pass
+
+    def test_environment_creation(self):
+        """
+        Checks if the creation of the environment is allowed or not and if parameters work correctly.
+        """
+        # TODO: think about tests
+        pass
+
+    def test_action_step(self):
+        """
+        Checks if an action step inside the environment works correctly.
+        """
+        # TODO: think about tests (what should be reached after specific action? what should happen if package is at
+        #  start point?)
+        pass
+
+    def test_reset_environment(self):
+        """
+        Checks if the reset of the environment works correctly.
+        """
+        # TODO: think about tests
+        pass
+
     def test_get_max_emission_action(self):
         """
         Checks if the maximum weight/emission baseline works correct.
         """
+        # TODO: think about tests (test one step, test all steps)
         pass
 
     def test_get_min_emission_action(self):
         """
         Checks if the minimum weight/emission baseline works correct.
         """
+        # TODO: think about tests (test one step, test all steps)
         pass
 
+    def test_stored_sarsa(self):
+        """
+        Checks if storing and loading of trained sarsa agent is correct.
+        """
+        # TODO: think about tests
+        pass
